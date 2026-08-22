f6843611c16f56f33c8f8293f8061b6e5c062436
Adamfatherofhumanity <dimagolubovich2008@gmail.com>
2026-08-22 14:23:18 +0300

    kvadratka v5
    Заменил глобальную переменную need_to_continue на локальную; Добавил вывод аргументов и корней при ошибке программы в вычислениях; Добавил функцию для проверки enum EquationErrors; Изменил функцию вывода ошибки, чтобы она принимала код ошибки и на этом основании выводила сообщение о ошибке; Изменил название функции Conclusion на PrintEnding; Создал функцию, которая пропускает остаток строки; Заменил форму записи обращения к элементу структуры по указателю на эту структуру.
    
    P. S. Файл лучше открывать в кодировке 1251.

--- main.cpp
+++ main.cpp
@@ -2,11 +2,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
+#include <ctype.h>
+#include <math.h>
 
 const double EPS = 0.000001;
-bool need_to_continue = true;
-
-// TODO: переделать выход из программы на return 0 в main (убрать exit)
 
 enum Comparison
 {
@@ -29,8 +28,8 @@ enum EquationErrors
     ERROR_A = 0,
     ERROR_B = 1,
     ERROR_C = 2,
-    ERROR_SO_MANY = 3,
-    NO_ERRORS = 4,
+    NO_ERRORS = 3,
+    ERROR_SO_MANY = 4,
     UNEXPECTED_ERROR = 5
 };
 
@@ -55,16 +54,19 @@ void SolutionsOfQuadraticEquations(const struct EquationArgs * const ptr_args, s
 void SolutionsOfLinealEquations(const struct EquationArgs * const ptr_args, struct EquationSolves * const ptr_solves);
 
 int VerificationOfEnteredData(struct EquationArgs * const ptr_args);
-void InvalidInput(char arg);
+void InvalidInput(int code_of_error);
 void InvalidAnswer(void);
 bool CheckString(void);
 
 void Greetings(void);
-void Conclusion(const struct EquationArgs * const ptr_args, const struct EquationSolves * const ptr_solves);
+void PrintEnding(const struct EquationArgs * const ptr_args, const struct EquationSolves * const ptr_solves);
 int ContinueOrStop(bool * ptr_need_to_continue);
 
 void CheckQuadraticRoots(const struct EquationArgs * const ptr_args, const struct EquationSolves * const ptr_solves);
 void CheckLinealRoots(const struct EquationArgs * const ptr_args, const struct EquationSolves * const ptr_solves);
+bool CheckEquationErrors(void);
+
+void SkipString(void);
 
 int main(void)
 {
@@ -74,15 +76,24 @@ int main(void)
 
     Greetings();
 
+    bool need_to_continue = true;
+
     while (need_to_continue)
     {
-        if (VerificationOfEnteredData(&args) == NO_ERRORS)
+        int code_of_error = VerificationOfEnteredData(&args);
+
+        if (code_of_error == NO_ERRORS)
         {
             SolutionsOfEquations(&args, &solves);
 
-            Conclusion(&args, &solves);
+            PrintEnding(&args, &solves);
 
             ContinueOrStop(&need_to_continue);
+        }
+
+        else
+        {
+            InvalidInput(code_of_error);
         }
     }
 
@@ -94,7 +105,7 @@ void SolutionsOfEquations(const struct EquationArgs * const ptr_args, struct Equ
     assert(ptr_args != NULL);
     assert(ptr_solves != NULL);
 
-    if (ComparisonOfFractalNumbers((*ptr_args).a, 0.0) == EQUAL)
+    if (ComparisonOfFractalNumbers(ptr_args->a, 0.0) == EQUAL)
     {
         SolutionsOfLinealEquations(ptr_args, ptr_solves);
     }
@@ -128,18 +139,18 @@ void SolutionsOfQuadraticEquations(const struct EquationArgs * const ptr_args, s
     assert(ptr_args != NULL);
     assert(ptr_solves != NULL);
 
-    double discriminant = (*ptr_args).b * (*ptr_args).b - 4 * (*ptr_args).a * (*ptr_args).c;
+    double discriminant = ptr_args->b * ptr_args->b - 4 * ptr_args->a * ptr_args->c;
 
     if (discriminant < 0)
     {
-        (*ptr_solves).number_of_solves = ZERO_SOLVES;
+        ptr_solves->number_of_solves = ZERO_SOLVES;
     }
 
     else if (ComparisonOfFractalNumbers(discriminant, 0.0) == EQUAL)
     {
-        (*ptr_solves).solve1 = (-(*ptr_args).b) / (2 * (*ptr_args).a);
-        (*ptr_solves).solve2 = (-(*ptr_args).b) / (2 * (*ptr_args).a);
-        (*ptr_solves).number_of_solves = ONE_SOLVE;
+        ptr_solves->solve1 = (-ptr_args->b) / (2 * ptr_args->a);
+        ptr_solves->solve2 = (-ptr_args->b) / (2 * ptr_args->a);
+        ptr_solves->number_of_solves = ONE_SOLVE;
 
         CheckQuadraticRoots(ptr_args, ptr_solves);
     }
@@ -148,9 +159,9 @@ void SolutionsOfQuadraticEquations(const struct EquationArgs * const ptr_args, s
     {
         double sqrt_discriminant = sqrt(discriminant);
 
-        (*ptr_solves).solve1 = (-(*ptr_args).b - sqrt_discriminant) / (2 * (*ptr_args).a);
-        (*ptr_solves).solve2 = (-(*ptr_args).b + sqrt_discriminant) / (2 * (*ptr_args).a);
-        (*ptr_solves).number_of_solves = TWO_SOLVES;
+        ptr_solves->solve1 = (-ptr_args->b - sqrt_discriminant) / (2 * ptr_args->a);
+        ptr_solves->solve2 = (-ptr_args->b + sqrt_discriminant) / (2 * ptr_args->a);
+        ptr_solves->number_of_solves = TWO_SOLVES;
 
         CheckQuadraticRoots(ptr_args, ptr_solves);
     }
@@ -161,23 +172,23 @@ void SolutionsOfLinealEquations(const struct EquationArgs * const ptr_args, stru
     assert(ptr_args != NULL);
     assert(ptr_solves != NULL);
 
-    if (ComparisonOfFractalNumbers((*ptr_args).b, 0.0) == EQUAL)
+    if (ComparisonOfFractalNumbers(ptr_args->b, 0.0) == EQUAL)
     {
-        if (ComparisonOfFractalNumbers((*ptr_args).c, 0.0) == EQUAL)
+        if (ComparisonOfFractalNumbers(ptr_args->c, 0.0) == EQUAL)
         {
-            (*ptr_solves).number_of_solves = INF_SOLVES;
+            ptr_solves->number_of_solves = INF_SOLVES;
         }
 
         else
         {
-            (*ptr_solves).number_of_solves = ZERO_SOLVES;
+            ptr_solves->number_of_solves = ZERO_SOLVES;
         }
     }
 
     else
     {
-        (*ptr_solves).solve1 = (-(*ptr_args).c) / (*ptr_args).b;
-        (*ptr_solves).number_of_solves = ONE_SOLVE;
+        ptr_solves->solve1 = (-ptr_args->c) / ptr_args->b;
+        ptr_solves->number_of_solves = ONE_SOLVE;
 
         CheckLinealRoots(ptr_args, ptr_solves);
     }
@@ -186,9 +197,11 @@ void SolutionsOfLinealEquations(const struct EquationArgs * const ptr_args, stru
 int VerificationOfEnteredData(struct EquationArgs * const ptr_args)
 {
     assert(ptr_args != NULL);
+    assert(CheckEquationErrors());
+
     int number_of_scanned_args = -1;
 
-    number_of_scanned_args = scanf("%lf %lf %lf", &((*ptr_args).a), &((*ptr_args).b), &((*ptr_args).c));
+    number_of_scanned_args = scanf("%lf %lf %lf", &(ptr_args->a), &(ptr_args->b), &(ptr_args->c));
 
     if (number_of_scanned_args == 3)
     {
@@ -198,59 +211,28 @@ int VerificationOfEnteredData(struct EquationArgs * const ptr_args)
         }
     }
 
-    switch (number_of_scanned_args)
-    {
-        case -1:
-            return ERROR_EOF;
-
-        case 0:
-            InvalidInput('a');
-            return ERROR_A;
-
-        case 1:
-            InvalidInput('b');
-            return ERROR_B;
-
-        case 2:
-            InvalidInput('c');
-            return ERROR_C;
-
-        case 3:
-            return NO_ERRORS;
-
-        case 4:
-            printf("Введены лишние аргументы. Введите данные заново "
-                   "(сначала коэффициент при x^2, потом при x, потом свободный): ");
-            scanf("%*[^\n]");
-            getchar();
-            return ERROR_SO_MANY;
-
-        default:
-            printf("Произошла непредвиденная ошибка.\n");
-            return UNEXPECTED_ERROR;
-
-    }
+    return number_of_scanned_args;
 }
 
-void Conclusion(const struct EquationArgs * const ptr_args, const struct EquationSolves * const ptr_solves)
+void PrintEnding(const struct EquationArgs * const ptr_args, const struct EquationSolves * const ptr_solves)
 {
     assert(ptr_args != NULL);
     assert(ptr_solves != NULL);
 
-    printf("У уравнения %.3lf * x^2 + %.3lf * x + %.3lf ", (*ptr_args).a, (*ptr_args).b, (*ptr_args).c);
+    printf("У уравнения %.3lf * x^2 + %.3lf * x + %.3lf ", ptr_args->a, ptr_args->b, ptr_args->c);
 
-    switch ((*ptr_solves).number_of_solves)
+    switch (ptr_solves->number_of_solves)
     {
         case ZERO_SOLVES:
             printf("нет решений.\n");
             break;
 
         case ONE_SOLVE:
-            printf("одно решение x = %.3lf.\n", (*ptr_solves).solve1);
+            printf("одно решение x = %.3lf.\n", ptr_solves->solve1);
             break;
 
         case TWO_SOLVES:
-            printf("два решения x1 = %.3lf и x2 = %.3lf.\n", (*ptr_solves).solve1, (*ptr_solves).solve2);
+            printf("два решения x1 = %.3lf и x2 = %.3lf.\n", ptr_solves->solve1, ptr_solves->solve2);
             break;
 
         case INF_SOLVES:
@@ -269,12 +251,33 @@ void Greetings(void)
            "Введи 3 числа (сначала коэффициент при x^2, потом при x, потом свободный): ");
 }
 
-void InvalidInput(char arg)
+void InvalidInput(int code_of_error)
 {
-    printf("Аргумент %c введён неверно. Введите данные заново "
-           "(сначала коэффициент при x^2, потом при x, потом свободный): ", arg);
-    scanf("%*[^\n]");
-    getchar();
+    if (code_of_error == ERROR_A || code_of_error == ERROR_B || code_of_error == ERROR_C)
+    {
+        printf("Аргумент %c введён неверно. Введите данные заново "
+               "(сначала коэффициент при x^2, потом при x, потом свободный): ", 97+code_of_error);
+        SkipString();
+    }
+
+    else if (code_of_error == ERROR_EOF)
+    {
+        printf("Конец файла.");
+        assert(false);
+    }
+
+    else if (code_of_error == ERROR_SO_MANY)
+    {
+        printf("Введены лишние аргументы. Введите данные заново "
+               "(сначала коэффициент при x^2, потом при x, потом свободный): ");
+        SkipString();
+    }
+
+    else
+    {
+        printf("Произошла непредвиденная ошибка.\n");
+        assert(false);
+    }
 }
 
 bool CheckString(void)
@@ -300,6 +303,8 @@ bool CheckString(void)
 
 int ContinueOrStop(bool * ptr_need_to_continue)
 {
+    assert(ptr_need_to_continue != NULL);
+
     int answer= 0;
 
     printf("Хотите продолжить ввод (введите Y если да или N если нет): ");
@@ -339,13 +344,14 @@ void CheckQuadraticRoots(const struct EquationArgs * const ptr_args, const struc
     assert(ptr_args != NULL);
     assert(ptr_solves != NULL);
 
-    double value1 = (*ptr_args).a * (*ptr_solves).solve1 * (*ptr_solves).solve1 + (*ptr_args).b * (*ptr_solves).solve1 + (*ptr_args).c;
-    double value2 = (*ptr_args).a * (*ptr_solves).solve2 * (*ptr_solves).solve2 + (*ptr_args).b * (*ptr_solves).solve2 + (*ptr_args).c;
+    double value1 = ptr_args->a * ptr_solves->solve1 * ptr_solves->solve1 + ptr_args->b * ptr_solves->solve1 + ptr_args->c;
+    double value2 = ptr_args->a * ptr_solves->solve2 * ptr_solves->solve2 + ptr_args->b * ptr_solves->solve2 + ptr_args->c;
 
     if (ComparisonOfFractalNumbers(value1, 0.0) != EQUAL || ComparisonOfFractalNumbers(value2, 0.0) != EQUAL)
     {
         printf("Произошла ошибка в вычислениях.\n");
-        exit(EXIT_FAILURE);
+        printf("a = %lf, b = %lf, c = %lf, x1 = %lf, x2 = %lf.\n", ptr_args->a, ptr_args->b, ptr_args->c, ptr_solves->solve1, ptr_solves->solve2);
+        assert(false);
     }
 }
 
@@ -354,18 +360,29 @@ void CheckLinealRoots(const struct EquationArgs * const ptr_args, const struct E
     assert(ptr_args != NULL);
     assert(ptr_solves != NULL);
 
-    double value = (*ptr_args).b * (*ptr_solves).solve1 + (*ptr_args).c;
+    double value = ptr_args->b * ptr_solves->solve1 + ptr_args->c;
 
     if (ComparisonOfFractalNumbers(value, 0.0) != EQUAL)
     {
         printf("Произошла ошибка в вычислениях.\n");
-        exit(EXIT_FAILURE);
+        printf("b = %lf, c = %lf, x = %lf.\n", ptr_args->b, ptr_args->c, ptr_solves->solve1);
+        assert(false);
     }
 }
 
 void InvalidAnswer(void)
 {
     printf("Принимаются только ответы Y/N (введите Y если да или N если нет): ");
+    SkipString();
+}
+
+bool CheckEquationErrors(void)
+{
+    return (ERROR_EOF == -1 && ERROR_A == 0 && ERROR_B == 1 && ERROR_C == 2 && NO_ERRORS == 3 && ERROR_SO_MANY == 4 && UNEXPECTED_ERROR == 5);
+}
+
+void SkipString(void)
+{
     scanf("%*[^\n]");
     getchar();
 }
