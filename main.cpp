314a9857ce5c29c28cf795eadf7a1546ff8ad4b6
Adamfatherofhumanity <dimagolubovich2008@gmail.com>
2026-08-23 00:27:12 +0300

    kvadravka+unit-testing v2
    Доработал систему unit-тестирования, повысил качество кода.
    
    P.S. Вот пример правильного ввода: name_of_file test (если не указать test 2 аргументом, то программа запустится в пользовательском режиме) number_of_random_tests (по умолчанию 1000, можно задать своё значение) -v (вывод проверяемых аргументов, без флага не выводятся).
    
    P.P.S. Файл написан в кодировке 1251.

--- main.cpp
+++ main.cpp
@@ -8,6 +8,10 @@
 
 const double EPS = 1e-6;
 
+const int MINIMUM_ARGS_FOR_TEST = 2;
+const int NUMBER_OF_SPECIAL_TESTS = 8;
+const double ACCURACY = 1000;
+
 enum Comparison
 {
     BIGGER = 1,
@@ -55,7 +59,7 @@ void SolutionsOfQuadraticEquations(const struct EquationArgs * const ptr_args, s
 void SolutionsOfLinealEquations(const struct EquationArgs * const ptr_args, struct EquationSolves * const ptr_solves);
 
 int VerificationOfEnteredData(struct EquationArgs * const ptr_args);
-void InvalidInput(int code_of_error, bool * ptr_need_to_continue);
+void InvalidInput(const int code_of_error, bool * ptr_need_to_continue);
 void InvalidAnswer(void);
 bool CheckString(void);
 
@@ -69,46 +73,32 @@ bool CheckEquationErrors(void);
 
 void SkipString(void);
 
-void RunTests(struct EquationArgs * const ptr_args, struct EquationSolves * const ptr_solves, int number_of_random_tests);
+int StringToNumber(const char * string);
+
+void RunTests(struct EquationArgs * const ptr_args, struct EquationSolves * const ptr_solves, const int number_of_random_tests, const bool visible_args);
+void RunSpecialTests(struct EquationArgs * const ptr_args, struct EquationSolves * const ptr_solves, const bool visible_args);
+void RunRandomTests(struct EquationArgs * const ptr_args, struct EquationSolves * const ptr_solves, const int number_of_random_tests, const bool visible_args);
+
 void TestModeGreetings(void);
-int StringToNumber(char * string);
+void InvalidCommand(void);
+void PrintArgs(const struct EquationArgs * const ptr_args, const bool visible_args);
+
+void ClientProgram(struct EquationArgs * const ptr_args, struct EquationSolves * const ptr_solves);
+void TestingProgram(struct EquationArgs * const ptr_args, struct EquationSolves * const ptr_solves, const int argc, const char * const argv[]);
 
 int main(int argc, char * argv[])
 {
     EquationArgs args = { };
     EquationSolves solves = { };
 
-    if (argc == 3 && strcmp(argv[1], "test") == 0)
+    if (argc >= MINIMUM_ARGS_FOR_TEST && strcmp(argv[MINIMUM_ARGS_FOR_TEST-1], "test") == 0)
     {
-        int number_of_random_tests = StringToNumber(argv[2]);
-        TestModeGreetings();
-        RunTests(&args, &solves, number_of_random_tests);
+        TestingProgram(&args, &solves, argc, argv);
     }
 
     else
     {
-        Greetings();
-
-        bool need_to_continue = true;
-
-        while (need_to_continue)
-        {
-            int code_of_error = VerificationOfEnteredData(&args);
-
-            if (code_of_error == NO_ERRORS)
-            {
-                SolutionsOfEquations(&args, &solves);
-
-                PrintEnding(&args, &solves);
-
-                ContinueOrStop(&need_to_continue);
-            }
-
-            else
-            {
-                InvalidInput(code_of_error, &need_to_continue);
-            }
-        }
+        ClientProgram(&args, &solves);
     }
 
     return 0;
@@ -265,8 +255,10 @@ void Greetings(void)
            "Введи 3 числа (сначала коэффициент при x^2, потом при x, потом свободный): ");
 }
 
-void InvalidInput(int code_of_error, bool * ptr_need_to_continue)
+void InvalidInput(const int code_of_error, bool * ptr_need_to_continue)
 {
+    assert(ptr_need_to_continue != NULL);
+
     if (code_of_error == ERROR_A || code_of_error == ERROR_B || code_of_error == ERROR_C)
     {
         printf("Аргумент %c введён неверно. Введите данные заново "
@@ -403,47 +395,176 @@ void SkipString(void)
 
 void TestModeGreetings(void)
 {
-    printf("Начинаю unit-тестирование модуля SolutionsOfLinealEquations.\n");
+    printf("Начинаю unit-тестирование модуля SolutionsOfEquations.\n");
 }
 
-void RunTests(struct EquationArgs * const ptr_args, struct EquationSolves * const ptr_solves, int number_of_random_tests)
+void RunTests(struct EquationArgs * const ptr_args, struct EquationSolves * const ptr_solves, const int number_of_random_tests, const bool visible_args)
 {
+    assert(ptr_args != NULL);
+    assert(ptr_solves != NULL);
+
+    RunSpecialTests(ptr_args, ptr_solves, visible_args);
+
+    RunRandomTests(ptr_args, ptr_solves, number_of_random_tests, visible_args);
+
+    printf("Успешное тестирование. Пройдено %d/%d тестов.\n", number_of_random_tests+NUMBER_OF_SPECIAL_TESTS, number_of_random_tests+NUMBER_OF_SPECIAL_TESTS);
+}
+
+int StringToNumber(const char * str)
+{
+    assert(str != NULL);
+
+    for (int number_of_element = 0; number_of_element < int (strlen(str)); number_of_element++)
+    {
+        if (not isdigit(str[number_of_element]))
+        {
+            InvalidCommand();
+            return false;
+        }
+    }
+
+    return atoi(str);
+}
+
+void InvalidCommand(void)
+{
+    printf("Ошибка в аргументах командной строки. Вот пример правильного ввода:\n"
+           "name_of_file test number_of_random_tests (по умолчанию 1000, можно задать"
+           " своё значение) -v (вывод проверяемых аргументов, без флага не выводятся)");
+}
+
+void RunSpecialTests(struct EquationArgs * const ptr_args, struct EquationSolves * const ptr_solves, const bool visible_args)
+{
+    assert(ptr_args != NULL);
+    assert(ptr_solves != NULL);
+
     for (int first_arg = 0; first_arg < 2; first_arg++)
     {
-            for (int second_arg = 0; second_arg < 2; second_arg++)
+        for (int second_arg = 0; second_arg < 2; second_arg++)
         {
-                for (int third_arg = 0; third_arg < 2; third_arg++)
+            for (int third_arg = 0; third_arg < 2; third_arg++)
             {
-                ptr_args->a = first_arg;
-                ptr_args->b = second_arg;
-                ptr_args->c = third_arg;
-                *ptr_solves = { };
-                SolutionsOfEquations(ptr_args, ptr_solves);
+            ptr_args->a = first_arg;
+            ptr_args->b = second_arg;
+            ptr_args->c = third_arg;
+
+            PrintArgs(ptr_args, visible_args);
+
+            *ptr_solves = { };
+
+            SolutionsOfEquations(ptr_args, ptr_solves);
+            }
+        }
+    }
+}
+
+void RunRandomTests(struct EquationArgs * const ptr_args, struct EquationSolves * const ptr_solves, const int number_of_random_tests, const bool visible_args)
+{
+    assert(ptr_args != NULL);
+    assert(ptr_solves != NULL);
+
+    for (int iteration = 0; iteration < number_of_random_tests; iteration++)
+    {
+        ptr_args->a = (RAND_MAX/2-rand())/ACCURACY;
+        ptr_args->b = (RAND_MAX/2-rand())/ACCURACY;
+        ptr_args->c = (RAND_MAX/2-rand())/ACCURACY;
+
+        PrintArgs(ptr_args, visible_args);
+
+        *ptr_solves = { };
+
+        SolutionsOfEquations(ptr_args, ptr_solves);
+    }
+}
+
+void PrintArgs(const struct EquationArgs * const ptr_args, const bool visible_args)
+{
+    assert(ptr_args != NULL);
+
+    if (visible_args)
+    {
+        printf("a = %.3lf, b = %.3lf, c = %.3lf\n", ptr_args->a, ptr_args->b, ptr_args->c);
+    }
+}
+
+void ClientProgram(struct EquationArgs * const ptr_args, struct EquationSolves * const ptr_solves)
+{
+    assert(ptr_args != NULL);
+    assert(ptr_solves != NULL);
+
+    Greetings();
+
+    bool need_to_continue = true;
+
+    while (need_to_continue)
+    {
+        int code_of_error = VerificationOfEnteredData(ptr_args);
+
+        if (code_of_error == NO_ERRORS)
+        {
+            SolutionsOfEquations(ptr_args, ptr_solves);
+
+            PrintEnding(ptr_args, ptr_solves);
+
+            ContinueOrStop(&need_to_continue);
+        }
+
+        else
+        {
+            InvalidInput(code_of_error, &need_to_continue);
+        }
+    }
+}
+
+void TestingProgram(struct EquationArgs * const ptr_args, struct EquationSolves * const ptr_solves, const int argc, const char * const argv[])
+{
+    assert(ptr_args != NULL);
+    assert(ptr_solves != NULL);
+    assert(argv != NULL);
+
+    if (argc == MINIMUM_ARGS_FOR_TEST)
+    {
+        bool visible_args = false;
+        int number_of_random_tests = 1000;
+        TestModeGreetings();
+        RunTests(ptr_args, ptr_solves, number_of_random_tests, visible_args);
+    }
+
+    else if (argc == MINIMUM_ARGS_FOR_TEST+1)
+    {
+        if (strcmp(argv[MINIMUM_ARGS_FOR_TEST], "-v") == 0)
+        {
+            bool visible_args = true;
+            int number_of_random_tests = 1000;
+            TestModeGreetings();
+            RunTests(ptr_args, ptr_solves, number_of_random_tests, visible_args);
+        }
+
+        else
+        {
+            bool visible_args = false;
+
+            if (int number_of_random_tests = StringToNumber(argv[MINIMUM_ARGS_FOR_TEST])) //если поданная строка содержит что-то кроме цифр, то возвращается false
+            {
+                TestModeGreetings();
+                RunTests(ptr_args, ptr_solves, number_of_random_tests, visible_args);
             }
         }
     }
 
-    for (int iteration = 0; iteration < number_of_random_tests; iteration++)
+    else if (argc == MINIMUM_ARGS_FOR_TEST+2 && strcmp(argv[MINIMUM_ARGS_FOR_TEST+1], "-v") == 0)
     {
-        ptr_args->a = (16384-rand())/100;
-        ptr_args->b = (16384-rand())/100;
-        ptr_args->c = (16384-rand())/100;
-        *ptr_solves = { };
-        SolutionsOfEquations(ptr_args, ptr_solves);
+        bool visible_args = true;
+
+        if (int number_of_random_tests = StringToNumber(argv[MINIMUM_ARGS_FOR_TEST])) //если поданная строка содержит что-то кроме цифр, то возвращается false
+        {
+            TestModeGreetings();
+            RunTests(ptr_args, ptr_solves, number_of_random_tests, visible_args);
+        }
     }
 
-    printf("Успешное тестирование. Пройдено %d/%d тестов.\n", number_of_random_tests, number_of_random_tests);
-}
-
-int StringToNumber(char * str)
-{
-    int number = 0;
-    int length = strlen(str);
-
-    for (int digit_place = length-1; digit_place >= 0; digit_place--)
+    else
     {
-        number += int (pow(10, digit_place)) * (str[length-digit_place-1]-'0');
+        InvalidCommand();
     }
-
-    return number;
 }
